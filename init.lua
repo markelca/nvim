4c66f46b3889d3a846bdf24f235aa6101b4c1346
Markel Cuesta <cuestaarribas.markel@proton.me>
2025-01-24 11:21:26 +0100

    Added folding plugin

--- init.lua
+++ init.lua
@@ -26,6 +26,7 @@ require('lazy').setup({
   -- require 'plugins.image',
   -- require 'plugins.dap',
   -- require 'plugins.lint',
+  require 'plugins.ufo',
   { 'tpope/vim-sleuth' }, -- Detect tabstop and shiftwidth automatically
   { 'numToStr/Comment.nvim', opts = {} },
   { 'github/copilot.vim' },
