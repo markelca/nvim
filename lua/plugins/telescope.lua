b3a7043d01fb78f3f54fc0c6dc2ce74a1df6ef0e
Markel Cuesta <cuestaarribas.markel@proton.me>
2025-05-11 19:41:58 +0200

    clapi visibility param

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -31,6 +31,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
         },
         clapi = {
           show_inherited = true,
+          visibility = nil, -- Show all
         },
       },
     }
@@ -42,7 +43,6 @@ return { -- Fuzzy Finder (files, lsp, etc)
 
     -- See `:help telescope.builtin`
     local builtin = require 'telescope.builtin'
-    local extensions = require('telescope').extensions
     vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
     vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
     vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
@@ -54,7 +54,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
     vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
     vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
     vim.keymap.set('n', '<leader>st', vim.cmd.TodoTelescope, { desc = '[S]earch [T]odos' })
-    vim.keymap.set('n', '<leader>sa', extensions.clapi.clapi, { desc = '[S]earch [A]pi' })
+    vim.keymap.set('n', '<leader>sa', require('telescope').extensions.clapi.clapi, { desc = '[S]earch [A]pi' })
 
     -- Slightly advanced example of overriding default behavior and theme
     vim.keymap.set('n', '<leader>/', function()
